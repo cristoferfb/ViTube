1984176aef0aa88544f553dbddf69164b0ffbfe4
Cristofer Fuentes <contacto@cristoferfb.com>
2020-03-06 09:45:49 -0300

    add command mode

--- vitube.sh
+++ vitube.sh
@@ -35,19 +35,49 @@ _draw () { # Execute all draw jobs
 	done
 }
 
-_main_loop () {
+__execute_command () {
+	case "$1" in
+		"add")
+			echo "$2" >> ~/.config/vitube/subscriptions
+			;;	
+		"quit")
+			tput reset
+			exit 0
+			;;
+	esac
+}
+
+__command_mode () {
 	local _command
+	local _arg
+	tput cup "$(tput lines)" 0
+	tput setb 0
+	tput setf 7
+	read -rp ':' _command _arg
+	_draw # TODO avoid to redraw all TUI after read
+	__execute_command "$_command" "$_arg"
+}
+
+__read_key () {
+	case "$1" in
+		"q")
+			__execute_command "quit"
+			;;
+		":")
+			__command_mode
+			;;
+	esac
+}
+
+_main_loop () {
+	local _key
 	if $_draw_flag; then
 		_draw
 		_draw_flag=false
 	fi
-	read -rsn 1 _command
-	if [ "$_command" = "q" ]; then 
-		return 1
-	fi
+	read -rsn 1 _key
+	__read_key "$_key"
 }
 
 tput civis
-while true; do _main_loop || break; done
-tput reset
-exit 0
+while true; do _main_loop; done
